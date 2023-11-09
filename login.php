7075c3eba6fd339888c342c41e524cb14111120d
rafaax <rafavfx1@gmail.com>
2023-11-09 13:58:00 -0300

    unminify

--- login.php
+++ login.php
@@ -70,6 +70,6 @@ if (isset($_COOKIE['auth_token'])) {
 <!-- <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>  -->
 <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
 
-<script  src="assets/js/particles.min.js"></script>
+<script  src="assets/js/particles.js"></script>
 </body>
 </html>
