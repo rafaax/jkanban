1d63da16bf6e8a740deefa6869f029a64a553561
rafaax <rafavfx1@gmail.com>
2023-11-10 14:58:04 -0300

    span background color no progress

--- src/in_progress.php
+++ src/in_progress.php
@@ -7,7 +7,7 @@ file_put_contents('loginprogress.txt', file_get_contents("php://input"));
 $user = $json->user_id;
 
 
-$sql = "SELECT tp.tarefa_id, tc.titulo, tc.prioridade from tarefas_process tp 
+$sql = "SELECT tp.tarefa_id,tc.data_final, tc.titulo, tc.prioridade from tarefas_process tp 
     inner join tarefas_criadas tc on tc.tarefa_id = tp.tarefa_id
         where tc.usuario_tarefa = '$user'";
 // echo $sql;
@@ -16,8 +16,12 @@ $query = mysqli_query($conexao, $sql);
 $tarefas = [];
 
 while($array = mysqli_fetch_array($query)){
+    $titulo = $array['titulo'];
+    $data_final = $array['data_final'];
+    $titulo = "$titulo <span style='background-color:#f4ce46'>$data_final</span>";
+    
     $tarefas[] = [
-        'title' => $array['titulo'],
+        'title' => $titulo,
         'task_id' => $array['tarefa_id'],
         'prioridade' => $array['prioridade']
     ];
