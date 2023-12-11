b9d6ee90757777b92f97dbc63cc2907ea56f7974
rafaax <rafavfx1@gmail.com>
2023-12-11 11:41:32 -0300

    removendo pasta sql

--- sql/permissoes_ref.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `permissoes_ref` (
-	`id` INT(11) NULL DEFAULT NULL,
-	`desc` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci'
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
--- sql/tarefas_todo.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `tarefas_todo` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`tarefa_id` INT(11) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
