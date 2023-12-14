fc7fb5007a8507e252a697bda63084ab7f7aa777
rafaax <rafavfx1@gmail.com>
2023-12-14 10:06:17 -0300

    crialção do arquivo php

--- assets/js/contest-task.js
+++ assets/js/contest-task.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-    $('#form_cadastro').on("submit", function(event){
+    $('#contest_task').on("submit", function(event){
         event.preventDefault();
         $.ajax({
             method: "POST",
-            url: "src/insert_task.php",
+            url: "src/contest_task.php",
             data: new FormData(this),
             contentType: false,
             processData: false,
@@ -22,48 +22,48 @@ $(document).ready(function(){
             },
             success: function (result) {
                 console.log(result);
-                var json = JSON.parse(result);
-                Swal.close();
-                if(json.erro == false){
-                    let timerInterval;
-                    Swal.fire({
-                        icon: 'success',
-                        title: "Sucesso!",
-                        html: "Fechando em <b></b> milisegundos...",
-                        timer: 2000,
-                        timerProgressBar: true,
-                        didOpen: () => {
-                            Swal.showLoading();
-                            const timer = Swal.getPopup().querySelector("b");
-                            timerInterval = setInterval(() => {
-                            timer.textContent = `${Swal.getTimerLeft()}`;
-                            }, 100);
-                        },
-                        willClose: () => {
-                            clearInterval(timerInterval);
-                        }
-                    }).then((result) => {
-                    if (result.dismiss === Swal.DismissReason.timer) {
-                        window.location.href = "http://192.168.0.166/jkanban/";
-                    }
-                    });
-                }else if(json.erro == true){
-                    Swal.fire({
-                        title: json.msg,
-                        icon: 'error',
-                        allowOutsideClick: () => {
-                            const popup = Swal.getPopup()
-                            popup.classList.remove('swal2-show')
-                            setTimeout(() => {
-                            popup.classList.add('animate__animated', 'animate__headShake')
-                            })
-                            setTimeout(() => {
-                            popup.classList.remove('animate__animated', 'animate__headShake')
-                            }, 500)
-                            return false
-                        }
-                    })
-                }
+                // var json = JSON.parse(result);
+                // Swal.close();
+                // if(json.erro == false){
+                //     let timerInterval;
+                //     Swal.fire({
+                //         icon: 'success',
+                //         title: "Sucesso!",
+                //         html: "Fechando em <b></b> milisegundos...",
+                //         timer: 2000,
+                //         timerProgressBar: true,
+                //         didOpen: () => {
+                //             Swal.showLoading();
+                //             const timer = Swal.getPopup().querySelector("b");
+                //             timerInterval = setInterval(() => {
+                //             timer.textContent = `${Swal.getTimerLeft()}`;
+                //             }, 100);
+                //         },
+                //         willClose: () => {
+                //             clearInterval(timerInterval);
+                //         }
+                //     }).then((result) => {
+                //     if (result.dismiss === Swal.DismissReason.timer) {
+                //         window.location.href = "http://192.168.0.166/jkanban/";
+                //     }
+                //     });
+                // }else if(json.erro == true){
+                //     Swal.fire({
+                //         title: json.msg,
+                //         icon: 'error',
+                //         allowOutsideClick: () => {
+                //             const popup = Swal.getPopup()
+                //             popup.classList.remove('swal2-show')
+                //             setTimeout(() => {
+                //             popup.classList.add('animate__animated', 'animate__headShake')
+                //             })
+                //             setTimeout(() => {
+                //             popup.classList.remove('animate__animated', 'animate__headShake')
+                //             }, 500)
+                //             return false
+                //         }
+                //     })
+                // }
             }
         })
     });
